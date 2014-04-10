2cea658ae0b174801a377b963ee19b3c0c4a1554
harishbagewadielluru <harishbe88@yahoo.com>
2014-04-09 20:00:47 -0400

    generalized sript

--- script_to_pull_files.sh
+++ script_to_pull_files.sh
@@ -6,7 +6,7 @@ while read day
 do
 while read port
 do
-`scp -i /Users/harishbagewadielluru/harish_cloudwick_com_id_rsa -P $port rightscale@67.192.178.51:"/var/log/keyspace_size-$month\ $day.log" keyspace_size-$month$day-$port.log`
+`scp -i <rsa_key> -P $port <user>@<ip>:"/var/log/keyspace_size-$month\ $day.log" keyspace_size-$month$day-$port.log`
 done < $1
 done < $2
 done < $3
